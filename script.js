bb733b3cb4fc4ea006b7a43ec801a0b628781d3f
Jesus DeLintt <jesus.delintt@gmail.com>
2020-08-05 21:00:45 -0700

    started my for loop to change color boxes

--- script.js
+++ script.js
@@ -7,3 +7,6 @@ function showTime() {
   $("#currentDay").text(showTime);
 }
 setInterval(showTime, 1000);
+
+for (let t = startTime; t <= endTime; t++) {
+    const hourStr = `${t % 12 || 12}:00 ${t > 11 ? "PM" : "AM"}`;
