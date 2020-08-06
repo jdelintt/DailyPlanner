be829e744a91f6bf251d253c7443c3c25f667dd8
Jesus DeLintt <jesus.delintt@gmail.com>
2020-08-05 20:59:18 -0700

    created costants

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const timeBlockContainer = $(".container");
+const startTime = 8;
+const endTime = 17;
+
 function showTime() {
   var showTime = moment().format("dddd MMMM Do YYYY, h:mm:ss a");
   $("#currentDay").text(showTime);
