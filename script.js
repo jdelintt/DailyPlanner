f17dfb3d4583c7012958a1a694a8fcfa60af9d0e
Jesus DeLintt <jesus.delintt@gmail.com>
2020-08-05 21:19:10 -0700

    created const to change colors of text area

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function showTime() {
   $("#currentDay").text(showTime);
 }
 setInterval(showTime, 1000);
-
+const currentTime = moment().format("k");
 for (let t = startTime; t <= endTime; t++) {
   const hourStr = `${t % 12 || 12}:00 ${t > 11 ? "PM" : "AM"}`;
   const row = $("<div>").addClass("row time-block");
@@ -16,7 +16,12 @@ for (let t = startTime; t <= endTime; t++) {
   if (t < currentTime) textArea.addClass("past");
   else if (t === currentTime) textArea.addClass("present");
   else textArea.addClass("future");
-
+  textArea.val(localStorage.getItem(hourStr) || "");
+  const btn = $("<button>").addClass("saveBtn");
+  btn.on("click", function (event) {
+    event.preventDefault();
+    localStorage.setItem(hourStr, textArea.val());
+  });
   btn.append($("<i>").addClass("far fa-save"));
   row.append(hourElement, textArea, btn);
   timeBlockContainer.append(row);
